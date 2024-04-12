1a5087a96ad8f6b55753f6a063fe13fe889d75bf
Enoch Liu <TBENOCH@GMAIL.COM>
2024-04-11 20:22:27 -0400

    events updates

--- js/data/events-data.js
+++ js/data/events-data.js
@@ -13,138 +13,138 @@ export const EVENT_LIST = [
   },
   {
     date: "2023-06-15",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/230615.jpg",
     title: "HBS/Private Capital Project: Israel's Entrepreneurial Ecosystem",
-    url: "#"
+    url: "/assets/news/pdf/events/Summary Israeli VC 080523 UPDATED.pdf"
   },
   {
-    date: "2022-05-16",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    date: "2023-05-16",
+    image: "/assets/news/images/events/cover/230516.jpg",
     title: "HBS/Private Capital Project: The Measurement of Impact Investors in an Age of Skepticism",
     url: "/assets/news/pdf/events/Summary Impact Crossroads 06212023 UPDATED.pdf"
   },
   {
     date: "2022-12-14",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project/ITT Madras Webinar: The State of Indian Private Capital Industry",
     url: "/assets/news/pdf/events/Indian PE Summary FINAL.pdf"
   },
   {
     date: "2022-09-14",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: The Engagement of Private Equity with Employees of Portfolio Companies",
     url: "/assets/news/pdf/events/PE Employee FINAL.pdf"
   },
   {
     date: "2022-05-11",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/220511.jpg",
     title: "HBS/Private Capital Project Webinar: The Growth of European Venture capital",
     url: "/assets/news/pdf/events/Summary European VC UPDATED[45].pdf"
   },
   {
     date: "2022-03-22",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/220322.jpg",
     title: "HBS/Private Capital Project Webinar: The Challenges of 'Tough Tech': The American Venture System and National Competitiveness",
     url: "/assets/news/pdf/events/PE Tough Tech UPDATED.pdf"
   },
   {
     date: "2022-01-18",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: Public Market Replication of Private Equity",
     url: "/assets/news/pdf/events/PE Replication Summary FINAL.pdf"
   },
   {
     date: "2021-12-16",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: Private Equity and Healthcare: A Perfect Marriage?",
     url: "/assets/news/pdf/events/Summary Changing Secondary Market FINAL 12162021.pdf"
   },
   {
     date: "2021-06-18",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: The Changing Secondary Market",
     url: "/assets/news/pdf/events/Summary PE and health care 06182021 FINAL.pdf"
   },
   {
     date: "2021-06-16",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: FinTech and Small Business",
     url: "/assets/news/pdf/events/PCRI 061621 fintech summary FINAL.pdf"
   },
   {
     date: "2021-03-17",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/210317.jpg",
     title: "HBS/Private Capital Project Webinar: Riding the SPAC Wave",
     url: "/assets/news/pdf/events/Riding the SPAC Wave_Write-up.pdf"
   },
   {
     date: "2020-09-29",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "HBS/Private Capital Project Webinar: Impact Investing: How does it Fit into the Broader Capital Markets? Case Studies from the Forest Conservation Sector",
     url: "/assets/news/pdf/events/Forest_Conservation_WriteUp.20201114.pdf"
   },
   {
     date: "2020-09-09",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "PCRI/HBS Private Capital Project Webinar: The Future of Diversity and Inclusion in Private Equity: Can the Industry Meet the Challenge?",
     url: "/assets/news/pdf/events/Diversity and Inclusion Summary 10082020 final.pdf"
   },
   {
     date: "2020-07-15",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "PCRI/HBS Private Capital Project Webinar: VC Policy in the time of COVID-19",
     url: "/assets/news/pdf/events/Summary VC Policy in the time of Covid final.pdf"
   },
   {
     date: "2019-10-11",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "PCRI/HBS Private Capital Project Roundtable: The Rise of the Asset Owner-Investor in Private Markets",
     url: "/assets/news/pdf/events/PCRI & HBS_Asset Owner-Investor Summary_2019-Oct-11.pdf"
   },
   {
     date: "2019-06-21",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
-    title: "PCRI/PBC School of Finance ()Tsinghua University) Roundtable: China's Changing Private Market Landscape and its Emerging Investment Opportunites",
+    image: "/assets/news/images/events/cover/190621.png",
+    title: "PCRI/PBC School of Finance (Tsinghua University) Roundtable: China's Changing Private Market Landscape and its Emerging Investment Opportunites",
     url: "/assets/news/pdf/events/PCRI Tsinghua Summary_2019-06-21.pdf"
   },
   {
     date: "2018-09-11",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/180911.jpg",
     title: "PCRI/Impact Collaboratory at HBS Roundtable: The Rise of Impact Investing",
     url: "/assets/news/pdf/events/Summary of Roundtable Impact Sept 2018 11302018_final.pdf"
   },
 
   {
     date: "2018-06-15",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/180615.jpg",
     title: "PCRI/Berkeley Roundtable: New Models in Entrepreneurial Finance",
     url: "/assets/news/pdf/events/PCRI-Inst for Business Innovation-UC Haas-Summary_Jun 2018.pdf"
   },
 
   {
     date: "2017-09-06",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/170906.png",
     title: "HBS/ILPA/PCRI Roundtable — The Consequences of Private Equity on Employmnet and Management",
     url: "/assets/news/pdf/events/PCRI-ILPA-HBS Forum Summary_Sep 2017.pdf"
   },
 
   {
     date: "2017-06-13",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/events/cover/170613.jpg",
     title: "NDIGI/PCRI Roundtable — Has Persistence Persisted in Today's Private Equity",
-    url: "/assets/news/pdf/events/PCRI-NDIGI Forum Summary FINAL.pdf.pdf"
+    url: "/assets/news/pdf/events/PCRI-NDIGI Forum Summary FINAL.pdf"
   },
 
   {
     date: "2016-05-04",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "ILPA/PCRI Roundtable — Towards a Better Aligned Private Equity World: Perspectives of Limited and General Partners",
     url: "/assets/news/pdf/events/ILPA PCRI Roundtable May 2016 Final.pdf"
   },
 
   {
     date: "2015-04-01",
-    image: "/assets/news/images/gvs-2015/GSVApril104.jpg",
+    image: "/assets/news/images/pcri.jpg",
     title: "Driving Growth and Big Ideas: Private Capital's Role in Global Innovation (The PCRI and the Brookings Institution host a conference in Silicon Valley)",
     url: "/assets/news/pdf/events/April_1_2015_Conference_Press_Release.pdf"
   }
